14742df3c942255b4dfd20eca34599465be2442c
Sebastian Wålinder <s.walinder@gmail.com>
2018-11-21 21:23:20 +0100

    Added comment

--- exwm-firefox-evil.el
+++ exwm-firefox-evil.el
@@ -107,6 +107,7 @@ Firefox variant can be assigned in 'exwm-firefox-evil-firefox-name`"
 (define-key exwm-firefox-evil-mode-map [remap evil-substitute] 'exwm-firefox-evil-insert)
 (define-key exwm-firefox-evil-mode-map [remap evil-append]
   '(lambda () (interactive)
+     ;; When entering insert state via append the cursor moves to the right
      (exwm-firefox-core-right)
      (exwm-firefox-evil-insert)))
 
