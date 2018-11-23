3bb9ac6036b02d67bf6318418da32f15a71ec2b4
Sebastian Wålinder <s.walinder@gmail.com>
2018-11-23 14:22:38 +0100

    Attempting to emulate vim left and right on state change doesn't work well in all contextes

--- exwm-firefox-evil.el
+++ exwm-firefox-evil.el
@@ -94,12 +94,7 @@ Firefox variant can be assigned in 'exwm-firefox-evil-firefox-name`"
 
 ;;; Keys
 ;; Bind normal
-(define-key exwm-firefox-evil-mode-map [remap evil-normal-state]
-  '(lambda () (interactive)
-     ;; When exiting insert state the cursor moves to the left
-     (exwm-firefox-core-left)
-     (exwm-firefox-evil-normal)))
-
+(define-key exwm-firefox-evil-mode-map [remap evil-normal-state] 'exwm-firefox-evil-normal)
 (define-key exwm-firefox-evil-mode-map [remap evil-force-normal-state] 'exwm-firefox-evil-normal)
 (define-key exwm-firefox-evil-mode-map [remap evil-exit-visual-state]
   '(lambda () (interactive)
@@ -112,11 +107,7 @@ Firefox variant can be assigned in 'exwm-firefox-evil-firefox-name`"
 (define-key exwm-firefox-evil-mode-map [remap evil-insert-state] 'exwm-firefox-evil-insert)
 (define-key exwm-firefox-evil-mode-map [remap evil-insert] 'exwm-firefox-evil-insert)
 (define-key exwm-firefox-evil-mode-map [remap evil-substitute] 'exwm-firefox-evil-insert)
-(define-key exwm-firefox-evil-mode-map [remap evil-append]
-  '(lambda () (interactive)
-     ;; When entering insert state via append the cursor moves to the right
-     (exwm-firefox-core-right)
-     (exwm-firefox-evil-insert)))
+(define-key exwm-firefox-evil-mode-map [remap evil-append] 'exwm-firefox-evil-insert)
 
 ;;; Normal state
 ;; Basic movements
